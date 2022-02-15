5e6a09a4cd92b99f9c1b0c1373989641fd19e5bf
CesarCavalcanti <senadevtrd@gmail.com>
2022-02-14 23:23:28 -0300

    Implementando os beans estado e cidade e mapeando o relacionamento

---
+++ src/main/java/com/cavalcanti/apiappdelivery/model/entity/Cidade.java
@@ -0,0 +1,20 @@
+package com.cavalcanti.apiappdelivery.model.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cidade {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "estado_id")
+    private Estado estado;
+}
